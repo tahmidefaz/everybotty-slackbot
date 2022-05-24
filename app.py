606d46a5e2b4dc88ec9a2360f4d00855776c1127
tahmidefaz <tahmidefaz@gmail.com>
2022-05-24 16:40:05 -0400

    adding port for heroku

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ if __name__ == "__main__":
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
-    app.start(3000)
+    port = int(os.environ.get("PORT", "3000"))
+    app.start(port)
